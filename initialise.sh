358e18358602b31749faf92d934290a78e773adc
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2022-08-30 21:16:48 +0800

    Update initialise script

--- initialise.sh
+++ initialise.sh
@@ -8,11 +8,12 @@ fi
 # ARM64 -> AMD64 if needed:
 docker run --privileged tonistiigi/binfmt --install amd64
 
+# (Instruction 1)
 # Run the LibreTranslate instance, without a web UI:
-# The translate instance will be run on the same device
-# TODO: this may be a blocking call!
-docker run -it -p --restart=unless-stopped 5000:5000 libretranslate --disable-web-ui
+# The translate instance will be run on the same device.
 
+# (Instruction 2)
 # Start the bot through Poetry. Note that this call is blocking, do not place anything after it, unless it is intended to run after
 # the bot has finished operation.
-poetry run python3 bot.py
+
+docker run -it -p --restart=unless-stopped 5000:5000 libretranslate --disable-web-ui & poetry run python3 bot.py
