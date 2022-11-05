a866fc4b84fe7ab2a7bea9594351d2aa812aaaa6
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2022-11-05 17:35:32 +0800

    Delete translate function due to low usage

--- commands/misc.py
+++ commands/misc.py
@@ -28,66 +28,51 @@ from functions.io import json_open
 from ui.safeembed import SafeEmbed
 from functions.general import get_latency_ms, autocomplete_list
 
-# We will be disabling LibreTranslate until a host that is powerful enough is secured.
-lt_client = LibreTranslateAPI("https://translate.fortytwo-it.com/") # LibreTranslateAPI("https://localhost:5907")
-# Example: [{'code': 'en', 'name': 'English'}, {'code': 'ar', 'name': 'Arabic'}, {'code': 'zh', 'name': 'Chinese'}, {'code': 'fr', 'name': 'French'}, {'code': 'de', 'name': 'German'}, {'code': 'hi', 'name': 'Hindi'}, {'code': 'id', 'name': 'Indonesian'}, {'code': 'ga', 'name': 'Irish'}, {'code': 'it', 'name': 'Italian'}, {'code': 'ja', 'name': 'Japanese'}, {'code': 'ko', 'name': 'Korean'}, {'code': 'pl', 'name': 'Polish'}, {'code': 'pt', 'name': 'Portuguese'}, {'code': 'ru', 'name': 'Russian'}, {'code': 'es', 'name': 'Spanish'}, {'code': 'tr', 'name': 'Turkish'}, {'code': 'vi', 'name': 'Vietnamese'}]
-lt_raw_langs = lt_client.languages()
-lt_langs = {}
-for i in lt_raw_langs:
-    lt_langs[i["name"]] = i["code"]
-lt_source_langs = lt_langs.copy()
-lt_source_langs["Auto Detect"] = "auto"
-
-async def autocomplete_source_lang(ctx: discord.AutocompleteContext):
-    return await autocomplete_list(ctx, lt_source_langs.keys())
-
-async def autocomplete_dest_lang(ctx: discord.AutocompleteContext):
-    return await autocomplete_list(ctx, lt_langs.keys())
 
 class Misc(BaseCog):
 
     def __init__(self, bot):
         self.bot = bot
-    @commands.slash_command(name="translate", description="Translate between languages")
-    @discord.option("text", description="Text to translate")
-    @discord.option("source", description="Language you're translating from", autocomplete=autocomplete_source_lang)
-    @discord.option("dest", description="Language you're translating to", autocomplete=autocomplete_dest_lang)
-    async def translate(self, ctx: discord.ApplicationContext, text: str, source: str, dest: str):
-        await ctx.defer() # May take > 3 seconds
-        try:
-            if source not in lt_source_langs:
-                raise InvalidOptionException("Invalid source language!")
-            if dest not in lt_langs:
-                raise InvalidOptionException("Invalid target language!")
-            logging.info(f"/translate - from{lt_source_langs[source]} - to{lt_source_langs[dest]}")
-
-            translated = lt_client.translate(text,  lt_source_langs[source], lt_source_langs[dest])
-            embed = SafeEmbed(
-                title="Translated text",
-                description=translated
-            )
-            embed.safe_add_field("Original text", text)
-            embed.safe_add_field("From", source)
-            embed.safe_add_field("To", dest)
-            embed.set_footer(text="Powered by LibreTranslate")
-        except InvalidOptionException as err:
-            embed = SafeEmbed(
-                title="Error!",
-                description=str(err),
-                colour=discord.Colour.red()
-            )
-            logging.error(f"/translate: error occurred - {err} - {type(err)}")
-        except HTTPError as err:
-            embed = SafeEmbed(
-                title="Error!",
-                description="API endpoint error",
-                colour=discord.Colour.red()
-            )
-            logging.error(f"/translate: error occurred - {err} - {type(err)}")
-
-
-        await ctx.respond(embed=embed)
-
+    # @commands.slash_command(name="translate", description="Translate between languages")
+    # @discord.option("text", description="Text to translate")
+    # @discord.option("source", description="Language you're translating from", autocomplete=autocomplete_source_lang)
+    # @discord.option("dest", description="Language you're translating to", autocomplete=autocomplete_dest_lang)
+    # async def translate(self, ctx: discord.ApplicationContext, text: str, source: str, dest: str):
+    #     await ctx.defer() # May take > 3 seconds
+    #     try:
+    #         if source not in lt_source_langs:
+    #             raise InvalidOptionException("Invalid source language!")
+    #         if dest not in lt_langs:
+    #             raise InvalidOptionException("Invalid target language!")
+    #         logging.info(f"/translate - from{lt_source_langs[source]} - to{lt_source_langs[dest]}")
+    #
+    #         translated = lt_client.translate(text,  lt_source_langs[source], lt_source_langs[dest])
+    #         embed = SafeEmbed(
+    #             title="Translated text",
+    #             description=translated
+    #         )
+    #         embed.safe_add_field("Original text", text)
+    #         embed.safe_add_field("From", source)
+    #         embed.safe_add_field("To", dest)
+    #         embed.set_footer(text="Powered by LibreTranslate")
+    #     except InvalidOptionException as err:
+    #         embed = SafeEmbed(
+    #             title="Error!",
+    #             description=str(err),
+    #             colour=discord.Colour.red()
+    #         )
+    #         logging.error(f"/translate: error occurred - {err} - {type(err)}")
+    #     except HTTPError as err:
+    #         embed = SafeEmbed(
+    #             title="Error!",
+    #             description="API endpoint error",
+    #             colour=discord.Colour.red()
+    #         )
+    #         logging.error(f"/translate: error occurred - {err} - {type(err)}")
+    #
+    #
+    #     await ctx.respond(embed=embed)
+    #
 
 
     @commands.slash_command(name="ping", description="Ping the bot")
